8bdc80ee104f0babd7518e67ff223b0327ee3a2f
Joao2k23 <joaombraga@outlook.com.br>
2025-07-29 11:08:04 -0300

    criação da primeira rota de alunos e nova migrate

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
   async index(req,res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Jao',
-      sobrenome: 'braga',
-      email: 'joao@gmail.com',
-      idade: 112,
-      peso: 300,
-      altura: 1.81
-    })
-    res.json(novoAluno)
+    res.json('OK')
   }
 }
 
