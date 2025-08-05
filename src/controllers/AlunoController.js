924de799c4813ef765ee8bd9b0c70df48ce97812
Joao2k23 <joaombraga@outlook.com.br>
2025-08-05 09:43:12 -0300

    FEAT: Listando os alunos juntamente com suas respectivas fotos.

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,9 +1,18 @@
 import Aluno from '../models/Aluno'
+import Foto from '../models/Foto'
 
 class AlunoController {
   //index
   async index(req,res) {
-    const alunos = await Aluno.findAll()
+    const alunos = await Aluno.findAll({
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['filename'],
+      },
+    })
+
     res.json(alunos)
   }
 
@@ -62,7 +71,14 @@ class AlunoController {
         })
       }
 
-      const aluno = await Aluno.findByPk(id)
+      const aluno = await Aluno.findByPk(id, {
+      attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['filename'],
+      },
+    })
 
       if(!aluno) {
         return res.status(400).json({
